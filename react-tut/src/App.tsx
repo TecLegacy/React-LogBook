29640244d71671c7ba67c826bb4455ad9c28fe8b
TecLegacy <kshvkumar.kk2@gmail.com>
2023-02-13 21:09:22 +0530

    React-Router@latest

--- react-tut/src/App.tsx
+++ react-tut/src/App.tsx
@@ -1,23 +1,34 @@
-import AddUser from './components/project/add-user/AddUser';
-import ErrorModal from './components/project/errorModal/ErrorModal';
-import LoginOut from '@/components/context/LoginOut';
-import AuthProvider from '@/components/context/auth-context';
-import ForwardRef from '@/components/forward-ref/ForwardRef';
-import StarWarApi from './components/Api/StarWarApi';
-import MouseMove from '@/react-beta/MouseMove';
-import Button from './UI/Button';
-import Film from './components/Api/Film';
+// import AddUser from './components/project/add-user/AddUser';
+// import ErrorModal from './components/project/errorModal/ErrorModal';
+// import LoginOut from '@/components/context/LoginOut';
+// import AuthProvider from '@/components/context/auth-context';
+// import ForwardRef from '@/components/forward-ref/ForwardRef';
+// import StarWarApi from './components/Api/StarWarApi';
+// import MouseMove from '@/react-beta/MouseMove';
+// import Button from './UI/Button';
+// import Film from './components/Api/Film';
+// import Counter from './react-redux/Counter';
 import { useEffect, useState } from 'react';
-import Counter from './react-redux/Counter';
 import { useSelector } from 'react-redux';
 
-function App() {
+interface UserObj {
+  name: string;
+  age: number;
+}
+
+const App: React.FC = () => {
   const [data, setData] = useState(null);
   const showConsole = () => {
     console.log(data);
   };
 
-  const counter = useSelector((state) => state.counter);
+  // test object with typescript
+  const userName:<UserObj>  {
+    name: 'k',
+    age: 20,
+  };
+  const counter = useSelector((state: any) => state.counter);
+
   return (
     <>
       {/* REACT_BETA */}
@@ -46,10 +57,10 @@ function App() {
       {/* <Film data={data} /> */}
 
       {/* -------REDUX------------ */}
-      <h1>{counter}</h1>
-      <Counter />
+      {/* <h1>{counter}</h1>
+      <Counter /> */}
     </>
   );
-}
+};
 
 export default App;
