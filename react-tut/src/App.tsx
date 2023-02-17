f4ce825fa76cf0fcec7764125d9552a0c4b9e8dc
TecLegacy <kshvkumar.kk2@gmail.com>
2023-02-17 23:59:46 +0530

    Fetching with loader

--- react-tut/src/App.tsx
+++ react-tut/src/App.tsx
@@ -26,7 +26,46 @@ const Router = createBrowserRouter([
       { path: '', element: <Home /> },
       // { path: 'product', element: <Product /> ,},
       { path: 'items/:itemId', element: <Item /> }, //Dynamic and nested Routes
-      { path: 'items/new', element: <NewEventPage /> },
+      //Fetching star war api data
+      {
+        path: 'items/new',
+        element: <NewEventPage />,
+        loader: async () => {
+          const response = await fetch('https://swapi.dev/api/films');
+          try {
+            if (!response.ok) {
+              // Typescript conversion of code - Error method is of type string
+              // & response.status is type of number
+              throw new Error(response.status.toString());
+            }
+
+            const data = await response.json();
+
+            return data;
+          } catch (error: any) {
+            let errorMessage;
+            switch (error.message) {
+              case 'Failed to fetch':
+                errorMessage =
+                  'Unable to fetch data. Please check your internet connection.';
+                break;
+              case '404':
+                errorMessage = 'The user you requested does not exist.';
+                break;
+              case '500':
+                errorMessage = 'Server error. Please try again later.';
+                break;
+              default:
+                errorMessage = 'An error occurred. Please try again later.';
+                break;
+            }
+
+            throw new Error(errorMessage);
+          }
+
+          //
+        },
+      },
       { path: 'items/:someId/edit', element: <EditEventPage /> },
     ],
   },
